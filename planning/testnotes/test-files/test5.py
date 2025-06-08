b8aa40152ba77dd0a393646544719ee8690c6a11
Azriel Valerius Atienza Valdepenas <165470058+aectivist@users.noreply.github.com>
2025-06-08 19:12:32 +0800

    Archived plenty of things, and finished TTS

--- planning/testnotes/test-files/test5.py
+++
@@ -1,4 +0,0 @@
-base_path = r"C:\Users\aecti\OneDrive\Desktop\Projects\NAVI-AI\planning\dataset_folder\wavs"
-paths = [f"r'{base_path}\\{i}.wav'" for i in range(1, 225)]
-output_line = ", ".join(paths)
-print(output_line)
